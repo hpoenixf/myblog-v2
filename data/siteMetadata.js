f0bdd2d579525a0963cbbc9927988bbf39d77670
hpoenixf <hpoenixf@hpoenixfdeMacBook-Pro.local>
2025-01-28 17:58:58 +0800

    feat: build

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -63,14 +63,14 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'hpoenixf/myblog-v2',
+      repositoryId: 'R_kgDONvyQTA',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDONvyQTM4CmZ74',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
+      metadata: '1',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
       theme: 'light',
