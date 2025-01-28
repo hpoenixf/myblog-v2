352ddccb817f49d2ea777698fb8d7b947eb74985
hpoenixf <hpoenixf@hpoenixfdeMacBook-Pro.local>
2025-01-28 17:53:04 +0800

    feat: update config

--- .contentlayer/generated/Blog/_index.mjs
+++ .contentlayer/generated/Blog/_index.mjs
@@ -11,6 +11,7 @@ import blog__dataLogMdx from './blog__data-log.mdx.json' with { type: 'json' }
 import blog__designEverdayNoteMdx from './blog__design-everday-note.mdx.json' with { type: 'json' }
 import blog__feDesignMdx from './blog__fe-design.mdx.json' with { type: 'json' }
 import blog__feKnowledgeMdx from './blog__fe-knowledge.mdx.json' with { type: 'json' }
+import blog__fePerformanceMdx from './blog__fe-performance.mdx.json' with { type: 'json' }
 import blog__feSystemDesignImproveMdx from './blog__fe-system-design-improve.mdx.json' with { type: 'json' }
 import blog__frameSyncMdx from './blog__frame-sync.mdx.json' with { type: 'json' }
 import blog__goodSdkMdx from './blog__good-sdk.mdx.json' with { type: 'json' }
@@ -32,4 +33,4 @@ import blog__simpleVirtualDomMdx from './blog__simple-virtual-dom.mdx.json' with
 import blog__thisClosureMdx from './blog__this-closure.mdx.json' with { type: 'json' }
 import blog__whyFeMdx from './blog__why-fe.mdx.json' with { type: 'json' }
 
-export const allBlogs = [blog__2yFeReviewMdx, blog__aboutfaceNoteMdx, blog__beautyCodeMdx, blog__cdnDnsMdx, blog__cleanCodeNoteMdx, blog__codeCompleteNodeMdx, blog__codeCompleteNode2Mdx, blog__dataLogMdx, blog__designEverdayNoteMdx, blog__feDesignMdx, blog__feKnowledgeMdx, blog__feSystemDesignImproveMdx, blog__frameSyncMdx, blog__goodSdkMdx, blog__httpStatusCodeMdx, blog__interviewFor_2yMdx, blog__iscrollLiteMdx, blog__miniprogramMdx, blog__my_2018Mdx, blog__my_2019Mdx, blog__my_2024Mdx, blog__myFeViewMdx, blog__netMdx, blog__performanceBeforeMdx, blog__pmbokNoteMdx, blog__pyramidRincipleNoteMdx, blog__reactCropperMdx, blog__simpleJsFeBeMdx, blog__simpleVirtualDomMdx, blog__thisClosureMdx, blog__whyFeMdx]
+export const allBlogs = [blog__2yFeReviewMdx, blog__aboutfaceNoteMdx, blog__beautyCodeMdx, blog__cdnDnsMdx, blog__cleanCodeNoteMdx, blog__codeCompleteNodeMdx, blog__codeCompleteNode2Mdx, blog__dataLogMdx, blog__designEverdayNoteMdx, blog__feDesignMdx, blog__feKnowledgeMdx, blog__fePerformanceMdx, blog__feSystemDesignImproveMdx, blog__frameSyncMdx, blog__goodSdkMdx, blog__httpStatusCodeMdx, blog__interviewFor_2yMdx, blog__iscrollLiteMdx, blog__miniprogramMdx, blog__my_2018Mdx, blog__my_2019Mdx, blog__my_2024Mdx, blog__myFeViewMdx, blog__netMdx, blog__performanceBeforeMdx, blog__pmbokNoteMdx, blog__pyramidRincipleNoteMdx, blog__reactCropperMdx, blog__simpleJsFeBeMdx, blog__simpleVirtualDomMdx, blog__thisClosureMdx, blog__whyFeMdx]
